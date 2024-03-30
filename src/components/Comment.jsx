c8b6402a63d011cd4f11bfce8f9cfb6ff3e1606f
Vigneswaran Printhan <94120170+Printhan3@users.noreply.github.com>
2024-03-30 02:11:32 -0700

    Update Comment.jsx

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,22 +1,36 @@
-// Comment.js
-import React from 'react';
+import React ,{useState}from 'react';
 import Reply from './Reply';
 
 function Comment({ comment }) {
-  const { text, replies, upvotes } = comment;
+  //const { text, replies, upvotes } = comment;
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState('');
+ 
+  const handleCommentSubmit = (event) => {
+    event.preventDefault();
+    addComment();
+  };
+  const addComment = () => {
+    if (newComment.trim() !== '') {
+      setComments([...comments, newComment]);
+      setNewComment('');
+    }
+  };
+
+ 
 
   return (
     <div className="comment">
-      <p>{text}</p>
-      <button>Upvote ({upvotes})</button>
-      {/* Form for replying to comments */}
-      <form>
-        <input type="text" placeholder="Write your reply..." />
+      <p>{comment}</p>
+      {/* <button>Upvote ({upvotes})</button> */}
+      {/* Form for replying to comments*/}
+      <form onSubmit={handleCommentSubmit}>
+        <input type="text" placeholder="Write your reply..." name="reply" />
         <button type="submit">Reply</button>
       </form>
       {/* Display replies */}
-      {replies.map((reply, index) => (
-        <Reply key={index} reply={reply} />
+      {comments.map((com, index) => (
+        <Reply key={index} reply={com} /> 
       ))}
     </div>
   );
